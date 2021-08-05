7878ddd52fdf674ea549870db40d34bbf9dbf5eb
ericcapiz <ericcapiz@gmail.com>
2021-08-05 17:35:15 -0500

    added register route to be

--- be/index.js
+++ be/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
+
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
@@ -11,14 +14,17 @@ mongoose.connect(process.env.MONGO_URL,{
     useCreateIndex: true,
 }).then(console.log("Connected to MOngoDB")).catch((err)=> console.log(err))
 
-app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.json({api:"API IS UP"})
 })
 
+app.use("/api/auth", authRoute);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log("API UP")
 })
+
+//demo login
+//username: demo, email: test@gmail.com, password: password
