af2e9a92cdadd46e4f24b676a47b5dd622dce37d
ericcapiz <ericcapiz@gmail.com>
2021-08-07 12:10:24 -0500

    rendering posts from api

--- fe/src/components/singlePost/SinglePost.js
+++ fe/src/components/singlePost/SinglePost.js
@@ -1,24 +1,39 @@
+import {useEffect,useState} from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 import './singlePost.css';
+import React from 'react';
 
 const SinglePost = () => {
+    const location = useLocation();
+    const path = location.pathname.split("/")[2];
+    const [post,setPosts] = useState({})
+
+    useEffect(()=>{
+        const getPost = async ()=>{
+            const res = await axios.get("/posts/" + path)
+            setPosts(res.data)
+        }
+        getPost()
+    },[path])
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8a2V5c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="post" className="singlePostImg" />
+                {post.photo && (
+                    <img src={post.photo} alt="post" />
+                    )}
                 <h1 className="singlePostTitle">
-                    Lost Keys
+                    {post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon fas fa-edit"></i>
                         <i className="singlePostIcon fas fa-trash-alt"></i>
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Posted By: <b>Eric</b></span>
-                    <span className="singlePostDate">1 hour ago</span>
+                    <span className="singlePostAuthor">Posted By: <b>{post.username}</b></span>
+                    <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
-                <p className="singlePostDesc">I lost my keys at the local park. There are 4 keys on the key ring along with the sports 
-                team key holder thing. Please if anyone finds them, email me ASAP at lostkeys@gmail.com. 
-                I can give a $100 reward.</p>
+                <p className="singlePostDesc">{post.desc}</p>
             </div>
         </div>
     )
--- fe/src/pages/homepage/HomePage.js
+++ fe/src/pages/homepage/HomePage.js
@@ -1,14 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homePage.css";
 
 const HomePage = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=>{
+        const fetchPosts = async () =>{
+            const res = await axios.get("/posts")
+            setPosts(res.data)
+        }
+        fetchPosts()
+    },[])
     return (
         <> 
             <Header/> 
                 <div className ="home">
-                    <Posts />
+                    <Posts posts={posts} />
                     <Sidebar />
                 </div>
         </>
