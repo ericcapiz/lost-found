6cac118ad88e1890ba3bd598ebaceaf9ca3f14d0
ericcapiz <ericcapiz@gmail.com>
2021-08-07 14:49:50 -0500

    fixed aauth error on be

--- be/routes/auth.js
+++ be/routes/auth.js
@@ -2,41 +2,38 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
 
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//Register
-router.post("/register", async (req,res)=>{
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPass = await bcrypt.hash(req.body.password, salt);
-        const newUser = User({
-            username: req.body.username, 
-            email: req.body.email, 
-            password: hashedPass,
-        })
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials!");
 
-        const user = await newUser.save();
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json(err);
-    }
-})
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong credentials!");
 
-//Login
-router.post("/login", async (req, res)=>{
-    try {
-        const user = await User.findOne({username: req.body.username})
-        !user && res.status(400).json("Username/Password Incorrect!")
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-        const validated = await bcrypt.compare(req.body.password, user.password)
-        !validated && res.status(400).json("Username/Password Incorrect!")
-
-        const {password, ...others} = user._doc;
-        res.status(200).json(others)
-    } catch (error) {
-        res.status(500).json(err);
-        
-    }
-})
-
-module.exports = router
+module.exports = router;
